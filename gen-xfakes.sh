3389bc2f6b43d1cf4be376cb8906ee31dd5a5850
James Grenning <james@grenning.net>
2019-09-10 09:05:30 -0500

    Fixed EXPLODING_FAKE_FOR macro to include prototype and function void arguements

--- gen-xfakes.sh
+++ gen-xfakes.sh
@@ -20,9 +20,10 @@ preambleCFakes()
 #include <stdio.h>
 #include <stdlib.h>
 
-#define EXPLODING_FAKE_FOR(f) void f { printf("BOOM! time to write a better fake for %s\n", __func__); exit(1); }
-#define NULL_VOID_FAKE_FOR(f) void f { }
-#define NULL_VALUE_FAKE_FOR(f, value_type, result) value_type f { return result; }
+#define BOOM_MESSAGE printf("BOOM! time to write a better fake for %s\n", __func__)
+#define EXPLODING_FAKE_FOR(f) void f(void); void f(void) { BOOM_MESSAGE; exit(1); }
+#define NULL_VOID_FAKE_FOR(f) void f(void); void f(void) {}
+#define NULL_VALUE_FAKE_FOR(value_type, f, result) value_type f(void); value_type f(void) { return result; }
 
 
 EOM
--- test/golden/myfakes-c.c
+++ test/golden/myfakes-c.c
@@ -17,9 +17,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define EXPLODING_FAKE_FOR(f) void f { printf("BOOM! time to write a better fake for %s\n", __func__); exit(1); }
-#define NULL_VOID_FAKE_FOR(f) void f { }
-#define NULL_VALUE_FAKE_FOR(f, value_type, result) value_type f { return result; }
+#define BOOM_MESSAGE printf("BOOM! time to write a better fake for %s\n", __func__)
+#define EXPLODING_FAKE_FOR(f) void f(void); void f(void) { BOOM_MESSAGE; exit(1); }
+#define NULL_VOID_FAKE_FOR(f) void f(void); void f(void) {}
+#define NULL_VALUE_FAKE_FOR(value_type, f, result) value_type f(void); value_type f(void) { return result; }
 
 
 EXPLODING_FAKE_FOR(log_it)
